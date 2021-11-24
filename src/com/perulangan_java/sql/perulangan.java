85d2a3ab3a4447df4cc3c87c6d2e2f4e85a8f476
ujangwahyudi141 <ujangwahyudii14@gmail.com>
2021-11-24 10:27:07 +0700

    first commit

---
+++ src/com/perulangan_java/sql/perulangan.java
@@ -0,0 +1,14 @@
+package com.perulangan_java.sql;
+
+public class perulangan {
+
+	public static void main(String[] args) {
+		int n = 8;
+		for(int i=1; i <= 10; i++)
+		{
+			System.out.printf("%d x %d = %d%n", n,i,n*i);
+		}
+
+	}
+
+}
